b547d5927a495801e6d6a996457ccd08e7eecbe0
honeoyerobotics3951 <36572163+honeoyerobotics3951@users.noreply.github.com>
2022-01-30 15:07:45 -0500

    Added To LowerArm Commands

--- 2022Robot/src/main/java/frc/robot/commands/LowerRearArms.java
+++ 2022Robot/src/main/java/frc/robot/commands/LowerRearArms.java
@@ -24,11 +24,15 @@ public class LowerRearArms extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    climber.runRearArms(-1);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    climber.runRearArms(0);
+  }
 
   // Returns true when the command should end.
   @Override
