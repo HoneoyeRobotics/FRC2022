41d294503facae82b244a737dfd67ec3aefcea1f
Keith Richardson <36552751+kjrichardson@users.noreply.github.com>
2022-01-30 13:57:07 -0500

    added drivetrain subsystem

--- 2022Robot/src/main/java/frc/robot/RobotContainer.java
+++ 2022Robot/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.DriveRobot;
+import frc.robot.subsystems.DriveTrain;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,13 +17,17 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  //declare subsystems
+  private DriveTrain driveTrain;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    //initialize subsystems
+    driveTrain = new DriveTrain();
+
+
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -43,6 +47,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return new DriveRobot(driveTrain);
   }
 }
