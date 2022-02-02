58abd0c6542c2991b080ffba93df888dd0524b1f
Keith Richardson <36552751+kjrichardson@users.noreply.github.com>
2022-02-01 20:20:25 -0500

    updated ball

--- 2022Robot/src/main/java/frc/robot/subsystems/Ball.java
+++ 2022Robot/src/main/java/frc/robot/subsystems/Ball.java
@@ -4,18 +4,38 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Ball extends SubsystemBase {
+  //declare motors
+  private VictorSPX shooterMotor;
+  private VictorSPX feederMotor;
+
   /** Creates a new Ball. */
-  public Ball() {}
+  public Ball() {
+    shooterMotor = new VictorSPX(Constants.CANID_ShooterMotor);
+    feederMotor = new VictorSPX(Constants.CANID_FeederMotor);
+  }
 
   public void deployBallPickup() {}
-  public void runFeeder(double speed) {}
+  public void runFeeder(double speed) {
+    
+    shooterMotor.set(ControlMode.PercentOutput, speed);
+  }
   public void runPickUp(double speed) {}
   
-  public void runShooter() {}
-  public void stopShooter() {}
+  public void runShooter() {
+    shooterMotor.set(ControlMode.PercentOutput, 0.60);
+  }
+  public void stopShooter() {
+    shooterMotor.set(ControlMode.PercentOutput, 0);
+  }
 
   @Override
   public void periodic() {
